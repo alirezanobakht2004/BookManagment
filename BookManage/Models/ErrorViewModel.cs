e19ab4d983f354b77fe3ecc70c7f31ec4156259a
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-09-24 11:12:43 +0330

    Add project files.

---
+++ BookManage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookManage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
