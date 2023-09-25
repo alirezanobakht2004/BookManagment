c68dba37bf4b6ee2aa9a1b68eba8a7094f621768
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-09-25 19:22:32 +0330

    database is created and connected

---
+++ BookManage/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookManage.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManage.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+        
+    }
+}
