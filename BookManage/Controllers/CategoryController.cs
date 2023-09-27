57252b7b29fa4c26e56b42d514d6d892da2be2f3
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-09-27 11:09:33 +0330

    Notifications are added

--- BookManage/Controllers/CategoryController.cs
+++ BookManage/Controllers/CategoryController.cs
@@ -39,6 +39,7 @@ namespace BookManage.Controllers
             {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
             }
 
@@ -75,6 +76,7 @@ namespace BookManage.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -110,6 +112,7 @@ namespace BookManage.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
          
